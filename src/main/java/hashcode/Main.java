a6a494d990d5d4493265819c82f61efcb858fb18
Dmytro Miroshnychenko <dmiroshnychenko@osdb.io>
2020-02-20 20:28:17 +0200

    read file

--- src/main/java/hashcode/Main.java
+++ src/main/java/hashcode/Main.java
@@ -11,10 +11,19 @@ public class Main {
 
     private static final String INPUT_FILE = "input.txt";
 
+
     public static void main(String[] args) throws IOException {
 
         List<String> input = FileUtils.readLines(new File(INPUT_FILE), Charset.defaultCharset());
-        System.out.println(input);
+
+        String firstLine = input.get(0);
+        String books = firstLine.substring(0, 1);
+        String librares = firstLine.substring(2, 3);
+        String deadline = firstLine.substring(4, 5);
+
+        System.out.println(books);
+        System.out.println(librares);
+        System.out.println(deadline);
 
     }
 
