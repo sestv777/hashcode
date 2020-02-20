1d0faead9c7aa39d7c52c2e931ed9c8925678742
Dmytro Miroshnychenko <dmiroshnychenko@osdb.io>
2020-02-20 20:29:59 +0200

    many books

--- src/main/java/hashcode/Main.java
+++ src/main/java/hashcode/Main.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class Main {
 
     private static final String INPUT_FILE = "input.txt";
+    private static final String MANY_BOOKS = "many_books.txt";
 
 
     public static void main(String[] args) throws IOException {
