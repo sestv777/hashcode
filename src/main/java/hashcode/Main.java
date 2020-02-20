a14a8696cb5990f1696785fa4590dd11f7f5a347
Dmytro Miroshnychenko <dmiroshnychenko@osdb.io>
2020-02-20 20:22:36 +0200

    read file

--- src/main/java/hashcode/Main.java
+++ src/main/java/hashcode/Main.java
@@ -1,8 +1,19 @@
 package hashcode;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String INPUT_FILE = "input.txt";
+
+    public static void main(String[] args) throws IOException {
+
+        String input = FileUtils.readFileToString(new File(INPUT_FILE), Charset.defaultCharset());
+        System.out.println(input);
 
     }
 
