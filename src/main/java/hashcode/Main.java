23071f3c305d252f921bd55864f4f7ad0b65b501
Dmytro Miroshnychenko <dmiroshnychenko@osdb.io>
2020-02-20 20:23:50 +0200

    read file

--- src/main/java/hashcode/Main.java
+++ src/main/java/hashcode/Main.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.List;
 
 public class Main {
 
@@ -12,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String input = FileUtils.readFileToString(new File(INPUT_FILE), Charset.defaultCharset());
+        List<String> input = FileUtils.readLines(new File(INPUT_FILE), Charset.defaultCharset());
         System.out.println(input);
 
     }
